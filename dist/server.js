18618079d9427f15ef8f5b13e4c6642d931b8b57
Salomao Rodrigues <salomao88@gmail.com>
2017-05-30 17:10:00 +0100

    Add flow

---
+++ dist/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+
+function square(n) {
+  return n * n;
+}
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
